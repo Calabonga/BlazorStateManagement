aff424f5c2417a815d30f586e742ccb3e8ee20fa
Calabonga <dev@calabonga.net>
2022-08-25 08:05:02 +0700

    StateMachine

---
+++ BlazorStateManagement/Calabonga.BlazorStateInStateMachine/Calabonga.BlazorStateInStateMachine/Program.cs
@@ -0,0 +1,14 @@
+using Calabonga.BlazorStateInStateMachine;
+using Calabonga.BlazorStateInStateMachine.Services;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<StateService>();
+
+await builder.Build().RunAsync();
---
+++ BlazorStateManagement/Calabonga.BlazorStateInStateMachine/Calabonga.BlazorStateInStateMachine/Services/StateService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Calabonga.BlazorStateInStateMachine.Services
+{
+    public class StateService
+    {
+        public int Count { get; private set; }
+
+        public event Action<ComponentBase, int>? CounterChanged;
+
+        public int IncrementCounter(ComponentBase sender)
+        {
+            Count++;
+            NotifyCounterChanged(sender, Count);
+            return Count;
+        }
+
+        private void NotifyCounterChanged(ComponentBase sender, int count)
+        {
+            CounterChanged?.Invoke(sender, count);
+        }
+    }
+}
